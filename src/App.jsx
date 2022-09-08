df505eae18ce6a08acc7871086d9b0bcfc2c4780
coder <7346628+haoyunhao1@user.noreply.gitee.com>
2022-09-08 23:42:30 +0800

    react hooks - useMemo & useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import useApi from './hooks/useApi';
 
+function Child({callback}) {
+    useEffect(() => {
+        callback()
+    }, [callback])
+    return <div>子组件</div>
+}
+
 function  App() {
-    const [{data}, handleInputChange] = useApi()
+    // const [{data}, handleInputChange] = useApi()
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(18);
+    const [kw, setkw] = useState('');
+    const callback = useCallback(() => {
+        console.log(111)
+    }, [name, age])
     return (
         <div className='app'>
-            <input type="text" onChange={handleInputChange}/>
-            {data.map((item, index) => (
-                <div key={index}>{item}</div>
-            ))}
+            <input type="text" onChange={(e) => setName(e.target.value)}/>
+            <input type="text" onChange={(e) => setAge(e.target.value)}/>
+            <input type="text" onChange={(e) => setkw(e.target.value)}/>
+            <Child callback={callback}/>
         </div>
     )
 }
